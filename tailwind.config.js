cc0b4c3f8558903aaefb0c732efe3e66565e463e
guhzzk <rodrigues.freitas.gustavo@escola.pr.gov.br>
2026-05-14 11:31:55 -0300

    Add Tailwind CSS configuration with custom animations

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate3d(0,0,0)' },
+          '50%': { transform: 'translate3d(0,-20px,0)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
